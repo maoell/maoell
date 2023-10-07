3e61f0bc8b075ff7d358777704aff64e7cd8dd3b
maoell <111717398+maoell@users.noreply.github.com>
2023-10-07 12:29:43 +0700

    Add files via upload

---
+++ home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="maul/css/bootstrap.min.css">
+   
+</head>
+<body>
+    
+<div>
+
+ 
+<a href="{{ url('registrasi') }}">Registrasi</a> |
+<a href="{{ url('login') }}">Login</a> |
+<a href="{{ url('table') }}">Table</a> |
+
+
+</div>
+
+    <h1>{{$TextJudul}} </h1>
+    
+</body>
+</html>
